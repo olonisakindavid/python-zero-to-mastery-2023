86bb9698346a0d742bf7707bfe7f29225b1680ae
Olonisakin David <olonisakin.david@stu.cu.edu.ng>
2023-07-31 23:46:12 +0100

    basics 2

---
+++ Python Basics II/conditional_logic.py
@@ -0,0 +1,8 @@
+is_old = True
+is_licenced = True
+
+if is_old and is_licenced: #both here true
+    print('you are old enough to drive, and you have a licence!')
+# elif:
+else: 
+  print('you are not of age')
---
+++ Python Basics II/indentation_in_python.py
@@ -0,0 +1,8 @@
+is_old = True
+is_licenced = True
+#meaning to its identation, correctly indent for code to run
+if is_old and is_licenced: #both here true
+    print('you are old enough to drive, and you have a licence!')
+# elif:
+else: 
+  print('you are not of age')
